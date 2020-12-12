2837b88d6576290c40cbb13ead0eb1f8ab7ae9d5
HerbCaudill <herb@devresults.com>
2020-12-12 20:26:35 +0100

    App.tsx:

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
+import React, { useState } from 'react'
 import { Chooser } from './Chooser'
+import { Device } from './Device'
 
+interface Peer {
+  userName: string
+  deviceName: string
+}
+
+const initialPeers: Peer[] = [{ userName: 'Alice', deviceName: 'laptop' }]
 export const App = () => {
+  const [peers, setPeers] = useState<Peer[]>(initialPeers)
+
   return (
-    <div className="p-3">
+    <div className="flex p-3">
+      {peers.map((peer) => (
+        <Device {...peer}></Device>
+      ))}
       <Chooser></Chooser>
     </div>
   )
