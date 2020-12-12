557efa0560397e9f38365df69b5eba80ea4369bb
HerbCaudill <herb@devresults.com>
2020-12-12 18:58:46 +0100

    ✅

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,21 @@
+import React from 'react'
+import { Button, Card, CardBody, Input, Label } from '@windmill/react-ui'
+
 export const App = () => {
-  return <div className="bg-blue-500">asdf</div>
+  return (
+    <div className="p-3">
+      <Card className="max-w-sm">
+        <CardBody className="bg-teal-500 text-white text-2xl font-extrabold">Hello</CardBody>
+        <CardBody>
+          <Label className="text-xs">
+            <span>What is your name?</span>
+            <Input className="mt-1"></Input>
+          </Label>
+          <div className="my-3 text-right">
+            <Button>OK</Button>
+          </div>
+        </CardBody>
+      </Card>
+    </div>
+  )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { App } from './App'
+import { Windmill } from '@windmill/react-ui'
+import { theme } from './theme'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Windmill theme={theme}>
+      <App />
+    </Windmill>
   </React.StrictMode>,
   document.getElementById('root')
 )
