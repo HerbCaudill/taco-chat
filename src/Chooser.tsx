317fababb65c5c2715c93c69307cb1ae4ee6f862
HerbCaudill <herb@devresults.com>
2020-12-13 12:37:23 +0100

    Chooser.tsx:

--- src/Chooser.tsx
+++ src/Chooser.tsx
@@ -2,20 +2,20 @@ import { Button, Card, CardBody, Select } from '@windmill/react-ui'
 
 export const Chooser = () => {
   return (
-    <Card className="max-w-xs">
-      <CardBody>
-        <Select className="w-full my-3 h-10 text-lg">
+    <div className="px-3">
+      <div className="flex">
+        <Select className="w-full flex-grow my-3 mr-3 h-10 font-normal text-lg">
           <option>👩🏾 Alice</option>
           <option>👨🏻‍🦲 Bob</option>
           <option>👳🏽‍♂️ Charlie</option>
           <option>👴 Dwight</option>
         </Select>
-        <Select className="w-full my-3 h-10 text-lg">
-          <option>💻 Laptop</option>
-          <option>📱 Phone</option>
+        <Select className="w-full flex-1 min-w-24 my-3 h-10 text-lg">
+          <option>💻</option>
+          <option>📱</option>
         </Select>
-        <Button className="w-full block text-lg h-10">Add</Button>
-      </CardBody>
-    </Card>
+      </div>
+      <Button className="w-full block text-lg h-10">Add</Button>
+    </div>
   )
 }
