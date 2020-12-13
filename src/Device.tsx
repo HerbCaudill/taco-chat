3c42ed9d13f89b9f652000942790d1e22635a670
HerbCaudill <herb@devresults.com>
2020-12-13 12:37:33 +0100

    Device.tsx:

--- src/Device.tsx
+++ src/Device.tsx
@@ -1,16 +1,13 @@
 import { Card, CardBody, Label, Input, Button } from '@windmill/react-ui'
 
-export const Device = () => (
-  <Card className="max-w-sm">
-    <CardBody className="bg-teal-500 text-white text-2xl font-extrabold">Hello</CardBody>
-    <CardBody>
-      <Label className="text-xs">
-        <span>What is your name?</span>
-        <Input className="mt-1"></Input>
-      </Label>
-      <div className="my-3 text-right">
-        <Button className="min-w-24">OK</Button>
-      </div>
-    </CardBody>
+export const Device = ({ userName, deviceName }: DeviceProps) => (
+  <Card className="max-w-sm flex-1">
+    <CardBody className="bg-teal-500 text-white text-2xl font-extrabold">{userName}</CardBody>
+    <CardBody></CardBody>
   </Card>
 )
+
+interface DeviceProps {
+  userName: string
+  deviceName: string
+}
