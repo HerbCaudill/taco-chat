9ea0949e1246a682dd77d0c7d4f6aff0dd729be2
HerbCaudill <herb@devresults.com>
2020-12-14 14:23:32 +0100

    Chooser.tsx:

--- src/Chooser.tsx
+++ src/Chooser.tsx
@@ -1,4 +1,4 @@
-import { Button, Select } from '@windmill/react-ui'
+import { Select } from '@windmill/react-ui'
 import { useRef } from 'react'
 import { PeerMap } from './peers'
 
@@ -6,24 +6,23 @@ export const Chooser = ({ onAdd, peers }: ChooserProps) => {
   const peerSelect = useRef() as React.MutableRefObject<HTMLSelectElement>
 
   return (
-    <div className="px-3 flex">
-      <div className="w-64 flex-grow mr-3 ">
-        <Select ref={peerSelect} className="w-full h-10 font-normal text-lg" autoFocus={true}>
-          {Object.values(peers)
-            .filter(p => !p.added)
-            .map(p => (
-              <option key={p.id} value={p.id}>
-                {p.user.emoji} {p.device.emoji}
-              </option>
-            ))}
-        </Select>
-      </div>
-      <Button
-        className="w-full flex-0 block text-lg h-10"
-        onClick={() => onAdd(peerSelect.current.value)}
+    <div>
+      <Select
+        ref={peerSelect}
+        className="h-10 font-normal text-lg"
+        onChange={() => onAdd(peerSelect.current.value)}
       >
-        Add
-      </Button>
+        <option key={''} value={''}>
+          Add...
+        </option>
+        {Object.values(peers)
+          .filter(p => !p.added)
+          .map(p => (
+            <option key={p.id} value={p.id}>
+              {p.user.emoji} {p.device.emoji}
+            </option>
+          ))}
+      </Select>
     </div>
   )
 }
