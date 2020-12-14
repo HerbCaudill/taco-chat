7f4c3f44f0284a6d799f0aac2aa4e23d419234fe
HerbCaudill <herb@devresults.com>
2020-12-14 14:04:45 +0100

    App.tsx:

--- src/App.tsx
+++ src/App.tsx
@@ -6,21 +6,17 @@ import { peers as allPeers, PeerMap } from './peers'
 export const App = () => {
   const [peers, setPeers] = useState<PeerMap>(allPeers)
 
-  const onAdd = (id: string) => {
-    setPeers(peers => ({ ...peers, [id]: { ...peers[id], added: true } }))
-  }
-  const onRemove = (id: string) => {
-    setPeers(peers => ({ ...peers, [id]: { ...peers[id], added: false } }))
-  }
+  const setAdded = (v: boolean) => (id: string) =>
+    setPeers(peers => ({ ...peers, [id]: { ...peers[id], added: v } }))
 
   return (
     <div className="flex p-3 gap-3" style={{ minWidth: 2400 }}>
       {Object.values(peers)
         .filter(p => p.added)
         .map(p => (
-          <Peer key={p.id} onRemove={onRemove} {...p}></Peer>
+          <Peer key={p.id} onRemove={setAdded(false)} {...p}></Peer>
         ))}
-      <Chooser onAdd={onAdd} peers={peers}></Chooser>
+      <Chooser onAdd={setAdded(true)} peers={peers}></Chooser>
     </div>
   )
 }
