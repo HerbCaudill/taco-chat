4010f51d9775fbcca6996f202c79b478b2969ed2
HerbCaudill <herb@devresults.com>
2021-01-15 13:09:29 +0100

    Connection.ts:

--- src/Connection.ts
+++ src/Connection.ts
@@ -2,23 +2,22 @@ import * as auth from '@localfirst/auth'
 import { Duplex } from 'stream'
 import { WebSocketDuplex } from 'websocket-stream'
 import debug from 'debug'
-const log = debug('lf:tc:connection')
 
 export class Connection extends Duplex {
   authConnection: auth.Connection
+  log: debug.Debugger
 
   constructor(peerSocket: WebSocketDuplex, context: auth.InitialContext) {
     super()
-
     this._read = () => {}
     this._write = () => {}
+
+    this.log = debug(`lf:tc:connection:${context.user.userName}`)
 
     const authConnection = new auth.Connection(context)
     authConnection.start()
 
-    // this <-> authConnection <-> peerSocket
-    log({ peerSocket, authConnection })
-
+    // this ⇆ authConnection ⇆ peerSocket
     this.pipe(authConnection).pipe(this)
     authConnection.pipe(peerSocket).pipe(authConnection)
 
@@ -31,5 +30,9 @@ export class Connection extends Duplex {
 
   get team() {
     return this.authConnection.team
+  }
+
+  get state() {
+    return this.authConnection.state
   }
 }
