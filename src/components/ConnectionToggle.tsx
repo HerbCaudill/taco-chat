15959de068fd349202783fe67eaae3ab5412fcab
HerbCaudill <herb@devresults.com>
2021-01-18 14:48:31 +0100

    components/ConnectionToggle.tsx:

--- src/components/ConnectionToggle.tsx
+++ src/components/ConnectionToggle.tsx
@@ -1,14 +1,20 @@
 import React from 'react'
-import { useState } from 'react'
 import { Toggle } from './Toggle'
 
-export const ConnectionToggle: React.FC<ConnectionToggleProps> = ({ disabled = false }) => {
-  const [isConnected, setIsConnected] = useState(false)
-  const onConnectionToggle = () => {
-    setIsConnected(v => !v)
-  }
-  return <Toggle on={isConnected} disabled={disabled} onClick={onConnectionToggle}></Toggle>
-}
+export const ConnectionToggle: React.FC<ConnectionToggleProps> = ({
+  isConnected = false,
+  disabled = false,
+  onChange = () => {},
+}) => (
+  <Toggle //
+    on={isConnected}
+    disabled={disabled}
+    onClick={() => onChange(!isConnected)}
+  ></Toggle>
+)
+
 interface ConnectionToggleProps {
+  isConnected?: boolean
   disabled?: boolean
+  onChange?: (value: boolean) => void
 }
