a4f323b5f7afd2629d2e9856ca432e8a45aa92a9
HerbCaudill <herb@devresults.com>
2021-01-22 12:04:16 +0100

    ConnectionManager.ts:

--- src/ConnectionManager.ts
+++ src/ConnectionManager.ts
@@ -1,73 +1,103 @@
 ﻿import * as auth from '@localfirst/auth'
 import { Client } from '@localfirst/relay-client'
-import { EventEmitter } from 'events'
-import { Connection } from './Connection'
 import debug from 'debug'
-
-// TODO this probably belongs in the relay package - this would be the client we import?
+import { WebSocketDuplex } from 'websocket-stream'
+import { Connection } from './Connection'
+import { EventEmitter } from './EventEmitter'
 
 /**
  * Wraps a Relay client and creates a Connection instance for each peer we connect to.
  */
 export class ConnectionManager extends EventEmitter {
+  private context: auth.InitialContext
+
   private client: Client
   private connections: Record<string, Connection> = {}
-  private log: debug.Debugger
 
+  /** This is a map of userName -> state, where `state` is the state summary
+   * from the auth.Connection machine, e.g. "connecting:authenticating". */
   public state: Record<string, string> = {}
+  urls: string[]
+  teamName: string
 
   constructor({ teamName, urls, context }: ConnectionManagerOptions) {
     super()
-    this.log = debug(`lf:tc:connmgr:${context.user.userName}`)
+    this.log = debug(`lf:tc:connection-manager:${context.user.userName}`)
+
+    this.context = context
+    this.urls = urls
+    this.teamName = teamName
 
     // connect to relay server
-    this.client = new Client({ id: context.user.userName, url: urls[0] })
-    // tell relay server we're interested in a specific team
-    this.client.join(teamName)
-
-    this.client.on('close', () => {
-      // disconnected from relay server
-      this.emit('close')
-    })
-
-    this.client.on('peer', ({ id, socket }) => {
-      if (socket) {
-        // connected to a new peer
-        const connection = new Connection(socket, context)
-        this.connections[id] = connection
-
-        connection.on('change', connectionState => {
-          this.state = {
-            ...this.state,
-            [id]: connectionState,
-          }
-          this.emit('change')
-        })
-
-        connection.on('connected', () => this.emit('connected', connection))
-        connection.on('disconnected', event => this.disconnect(id, event))
-      } else {
-        this.log('no socket')
-      }
-    })
+    this.client = this.connectServer()
   }
 
-  public disconnect = (id: string, event?: any) => {
-    if (this.connections[id]) this.connections[id].disconnect()
-    delete this.connections[id]
-    const { [id]: toDelete, ...rest } = this.state
-    this.state = rest
-    this.emit('disconnected', id, event)
+  private connectServer(): Client {
+    const client = new Client({ userName: this.context.user.userName, url: this.urls[0] })
+    // tell relay server we're interested in a specific team
+    client
+      .join(this.teamName)
+
+      .on('close', () => {
+        // disconnected from relay server
+        this.disconnectServer()
+      })
+
+      .on('peer.connect', ({ userName, socket }) => {
+        // connected to a new peer
+        if (socket) this.connectPeer(userName, socket)
+        else this.log('no socket')
+      })
+    return client
+  }
+
+  public async disconnectServer() {
+    const closeAllConnections = Object.keys(this.connections).map(peerId =>
+      this.disconnectPeer(peerId)
+    )
+    await Promise.all(closeAllConnections)
+    this.connections = {}
+    this.emit('close')
+  }
+
+  private connectPeer(userName: string, socket: WebSocketDuplex) {
+    this.log('peer.connect', userName)
+    const connection = new Connection(socket, this.context)
+    this.connections[userName] = connection
+
+    connection.on('change', connectionState => {
+      this.state = {
+        ...this.state,
+        [userName]: connectionState,
+      }
+      this.emit('change')
+    })
+
+    connection.on('connected', () => this.emit('connected', connection))
+    connection.on('disconnected', event => this.disconnectPeer(userName, event))
+  }
+
+  public disconnectPeer = (userName: string, event?: any) => {
+    // if we have this connection, disconnect it
+    if (this.connections[userName]) {
+      this.connections[userName].disconnect()
+      delete this.connections[userName]
+    }
+
+    // update our state object
+    this.state = {
+      ...this.state,
+      [userName]: 'disconnected',
+    }
+
+    // notify relay server
+    this.client.disconnect(userName)
+
+    this.emit('disconnected', userName, event)
   }
 
   public get connectionCount() {
     return Object.keys(this.connections).length
-  }
-
-  public async close() {
-    const closeAllConnections = Object.keys(this.connections).map(peerId => this.disconnect(peerId))
-    await Promise.all(closeAllConnections)
-    this.connections = {}
   }
 }
 
